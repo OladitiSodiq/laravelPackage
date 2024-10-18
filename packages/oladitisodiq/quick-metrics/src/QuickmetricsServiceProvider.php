95ccf77365a187dbb1d74f9b60f568bcc5d845ed
oladitisodiq <oladitisodiq@email.com>
2024-10-18 09:16:04 +0100

    Test laravel package

---
+++ packages/oladitisodiq/quick-metrics/src/QuickmetricsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oladitisodiq\Quickmetrics;
+
+use Illuminate\Support\ServiceProvider;
+
+Class QuickmetricsServiceProvider extends ServiceProvider{
+
+    Public function boot(){
+        dd('it works well done ');
+
+    }
+
+    public function register(){
+
+    }
+}
